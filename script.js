50e8dffdd7cedbfd14cde4980aeaf037e3262464
Valentyn Tykhomyrov <khomyart@gmail.com>
2021-12-29 10:48:37 +0200

    keep working on drag-and-drop system

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//There is 2 main functions: itemDropActions, itemDragActions
+//itemDragActions is a function wich in general adds to item some drag and drop features
+//itemDropActions is a function wich declares what will heppend to item when drag and drop action will end
+//and also there 1 config object wich should be edited if needed
+
 let CONFIG = {
     workZoneIdentifier: '#main_field',
     rootIremsIdentifier: '.drag-and-drop-item',
@@ -7,11 +12,20 @@ let CONFIG = {
 
 const   workZone = document.querySelector(CONFIG.workZoneIdentifier), // field or div where droped items are located
         rootItems = document.querySelectorAll(CONFIG.rootIremsIdentifier) // items wich are basis of items in workZone
+        debugButton = document.querySelector('#debug_button')
+
+        debugButton.onmousedown = () => {
+            elementsOfSchema.forEach(element => {
+                element.setAttribute('i-is-movable', 'false');
+            });
+        }
+
 
 let     elementsOfSchema = [], // array with movable items inside the work field
         newElementOfSchema,
         shiftX, shiftY,
-        isWorkFieldSelectedAsDropTarget = null,
+        workFieldSelectedAsDropTarget = null,
+        workSpaceItemSelectedAsDropTarget = null,
         dropTarget = null
         
 /**
@@ -32,13 +46,15 @@ function createNewItem(event, rootItem) {
     shiftY = event.pageY - rootItem.getBoundingClientRect().top;
 
     if (newElementOfSchema.getAttribute('i-type') == 'work_space') {
-        newElementOfSchema.className = 'work-space'
+        //newElementOfSchema is a work-space-head and work-space-container holder
+        newElementOfSchema.className = 'work-space';
+        newElementOfSchema.childNodes[1].style = null;
+        //acceapt work-space-head class to first div (image holder)
+        newElementOfSchema.childNodes[1].className = 'work-space-head';
     } else {
         newElementOfSchema.className = 'field-item'
     }
 
-    //set this variable to '' as default value for every newly created item
-    isWorkFieldSelectedAsDropTarget = null;
     //set newly created item opacity to value wich is mentioned in config as itemOpacityWhileMoving
     newElementOfSchema.style.opacity = CONFIG.itemOpacityWhileMoving;
 }
@@ -59,22 +75,23 @@ function moveAt(mouseEvent, movableItem, itemOffsetX, itemOffsetY) {
 /**
  * Decides how item should behave when mouse moving is done and lmb has been released
  * @param {*} item 
- * @param {*} isWorkFieldSelectedAsDropTarget 
+ * @param {*} workFieldSelectedAsDropTarget 
  * @param {*} isItemNewlyCreatedFromRoot 
  */
-function itemEndpointBehavior(item, isWorkFieldSelectedAsDropTarget) {
+function itemDropActions(item, workSpaceItemDropType, workFieldSelectedAsDropTarget, workSpaceItemSelectedAsDropTarget) {
     item.style.opacity = 1;
     item.style.zIndex = '1000'
     item.style.transition = `all ease-in-out 0s`;
+    item.setAttribute('i-is-selectable', 'true');
 
-    if (isWorkFieldSelectedAsDropTarget == null 
+    if (workFieldSelectedAsDropTarget == null 
     && (workZone.getBoundingClientRect().left > item.getBoundingClientRect().right
     || workZone.getBoundingClientRect().right < item.getBoundingClientRect().left 
     || workZone.getBoundingClientRect().top > item.getBoundingClientRect().bottom
     || workZone.getBoundingClientRect().bottom < item.getBoundingClientRect().top)) {
         console.log('item should be deleted')
         item.remove();
-    }
+    } 
 
     if ((workZone.getBoundingClientRect().left > item.getBoundingClientRect().left) 
     || (workZone.getBoundingClientRect().right < item.getBoundingClientRect().right)
@@ -102,6 +119,23 @@ function itemEndpointBehavior(item, isWorkFieldSelectedAsDropTarget) {
         item.style.top = workZone.getBoundingClientRect().bottom - item.offsetHeight + 'px';
     }
 
+    if (workSpaceItemSelectedAsDropTarget) {
+        if (item.getAttribute('i-type') == 'work_space') {
+            
+        } else if (workSpaceItemDropType == 0) {
+            workSpaceItemSelectedAsDropTarget.childNodes[3].appendChild(item);
+            workSpaceItemSelectedAsDropTarget.childNodes[3].style.display = 'flex';
+            item.style = null;
+            item.className = 'work-space-container-item';
+        } else if (workSpaceItemDropType == 1) {
+            workSpaceItemSelectedAsDropTarget.parentNode.childNodes[3].appendChild(item);
+            workSpaceItemSelectedAsDropTarget.parentNode.childNodes[3].style.display = 'flex';
+            item.style = null;
+            item.className = 'work-space-container-item';
+        }
+        //workSpaceItemSelectedAsDropTarget.parentNode.childNodes[1].style.borderTopRightRadius = '0px';
+        //workSpaceItemSelectedAsDropTarget.parentNode.childNodes[1].style.borderBottomRightRadius = '0px';
+    }
     document.body.onmousemove = null;
     document.body.onmouseup = null;
 }
@@ -111,31 +145,103 @@ function itemEndpointBehavior(item, isWorkFieldSelectedAsDropTarget) {
  * @param {selected item} item 
  */
 function addDragAndDropToItem(item) {
-    item.onmousedown = (event) => {
-        item.ondragstart = () => false
-        
-        item.onmouseup = () => {
-            showContextMenuOfItem(item)
-        }
-        
-        shiftX = event.pageX - item.getBoundingClientRect().left,
-        shiftY = event.pageY - item.getBoundingClientRect().top;
-
-        document.body.onmousemove = (event) => {
-            moveAt(event, item, shiftX, shiftY);
-            item.style.opacity = CONFIG.itemOpacityWhileMoving;
-            item.style.display = 'none';
-            isWorkFieldSelectedAsDropTarget = document.elementFromPoint(event.clientX, event.clientY).closest('#main_field');
-            item.style.zIndex = '2000';
-            item.style.display = 'flex';
-            item.onmouseup = null
-        }
-
-        document.body.onmouseup = () => {
-            itemEndpointBehavior(newElementOfSchema, isWorkFieldSelectedAsDropTarget)
-            document.body.onmousemove = null;
-        }        
+    item.onmousedown = () => {
+        itemDragActions(item, event)
     }
+    itemDragActions(item, event)
+}
+
+/**
+ * actions wich happens while drag and drop event is going
+ * @param {item of actions} item 
+ */
+function itemDragActions(item) {
+    item.onmouseup = () => {
+        /**
+         * If workspace item has more than 1 child item and user wants to click on this child item
+         * if statement down below prevent workspace item context menu appearing
+         */
+        
+        if (item.className == 'work-space-container-item') {
+            item.parentNode.parentNode.setAttribute('i-is-selectable', 'false');
+        }
+
+        if(item.getAttribute('i-is-selectable') == 'true') {
+            showContextMenuOfItem(item);
+        }
+    }
+
+    //set this variable to null as default value for every newly created item
+    workFieldSelectedAsDropTarget = null;
+    workSpaceItemSelectedAsDropTarget = null;
+    workSpaceContainerItemSelectedAsMoveTarget = null;
+    
+    shiftX = event.pageX - item.getBoundingClientRect().left;
+    shiftY = event.pageY - item.getBoundingClientRect().top;
+
+    document.body.onmousemove = (event) => {
+        item.onmouseup = null
+        workSpaceContainerItemSelectedAsMoveTarget = 
+        document.elementFromPoint(event.clientX, event.clientY).closest('.work-space-container-item');
+        
+        if (workSpaceContainerItemSelectedAsMoveTarget) {
+            item = workSpaceContainerItemSelectedAsMoveTarget;
+            shiftX = event.pageX - item.getBoundingClientRect().left;
+            shiftY = event.pageY - item.getBoundingClientRect().top;
+            workSpaceContainerItemSelectedAsMoveTarget.className = 'field-item' 
+            workZone.append(workSpaceContainerItemSelectedAsMoveTarget);                             
+        }
+
+        /**
+         * If workspace item is empty and has no workspace container item childs
+         * set display to none for third workspace child (workspace item container)
+         */
+        elementsOfSchema.forEach(element => {
+            if (element.className == 'work-space' && 
+                element.style.display == 'flex' &&
+                element.childNodes[3].childNodes.length == 1) {    
+                element.childNodes[3].style.display = 'none';
+            }
+        });
+
+        moveAt(event, item, shiftX, shiftY);           
+        item.style.opacity = CONFIG.itemOpacityWhileMoving;
+        //To get information about what we have under the item wich moving with cursor, we should do checking when actual item 
+        //is invisible, so in this case we will get correct information for workFieldSelectedAsDropTarget variable
+        item.style.display = 'none';
+        workFieldSelectedAsDropTarget = 
+        document.elementFromPoint(event.clientX, event.clientY).closest('#main_field');
+        /**
+         * 0 for first use case, 1 for second use case
+         */
+        workSpaceItemDropType = 0;
+
+        /**
+         * Use this realisation if worspace drop target needed to be just entire div
+         */
+        workSpaceItemSelectedAsDropTarget = 
+        document.elementFromPoint(event.clientX, event.clientY).closest('.work-space');
+
+        /**
+         * Use realisation described down below if needed to separate drop target to 2 components such as work-space-head
+         * and work-space-item-container
+         */
+        /*
+        workSpaceItemSelectedAsDropTarget = 
+        document.elementFromPoint(event.clientX, event.clientY).closest('.work-space-head');
+        if(!workSpaceItemSelectedAsDropTarget) {
+            workSpaceItemSelectedAsDropTarget = 
+            document.elementFromPoint(event.clientX, event.clientY).closest('.work-space-item-container');
+        }
+        */   
+        item.style.zIndex = '2000';
+        item.style.display = 'flex';
+    }
+
+    document.body.onmouseup = () => {
+        itemDropActions(item, workSpaceItemDropType, workFieldSelectedAsDropTarget, workSpaceItemSelectedAsDropTarget);
+    }
+   
 }
 
 /**
@@ -143,7 +249,8 @@ function addDragAndDropToItem(item) {
  * @param {selected item} item 
  */
 function showContextMenuOfItem(item) {
-    console.log(item.getAttribute('i-type'));
+    //console.log(item.getAttribute('i-type'));
+    console.log(item)
 }
 
 //loop wich allows to accept all needed functions to every root item
@@ -152,18 +259,10 @@ for (let i = 0; i < rootItems.length; i++) {
         createNewItem(event, rootItems[i]);
         
         //Variable has been assigned in createNewItem function
-        newElementOfSchema.ondragstart = () => false;
         moveAt(event, newElementOfSchema, shiftX, shiftY);
         addDragAndDropToItem(newElementOfSchema);
         
-        document.body.onmousemove = (event) => {
-            moveAt(event, newElementOfSchema, shiftX, shiftY);
-            //To get information about what we have under the item wich moving with cursor, we should do checking when actual item 
-            //is invisible, so in this case we will get correct information for isWorkFieldSelectedAsDropTarget variable
-            newElementOfSchema.style.display = 'none';
-            isWorkFieldSelectedAsDropTarget = document.elementFromPoint(event.clientX, event.clientY).closest('#main_field');
-            newElementOfSchema.style.display = 'flex';
-        }
+        newElementOfSchema.ondragstart = () => false;
     }
 }
 
